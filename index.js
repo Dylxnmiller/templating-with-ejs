312d648538f796400870c6f0a86c2688f82102f5
Dylan Miller <91589107+Dylxnmiller@users.noreply.github.com>
2022-03-22 03:27:05 +0000

    added data

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+//here's our test data 
+var data = require('./data/test.json');
+
  
 app.set('view engine','ejs');
 
@@ -11,6 +15,25 @@ app.get('/', (req, res) => {
   var title = 'Our Home Page';
   res.render('pages/index',{'title':title});
 });
+
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
 
 app.get('/baseball', (req, res) => {
   var title = 'Baseball Page';
@@ -29,4 +52,5 @@ app.get('/football', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(data);
 });
